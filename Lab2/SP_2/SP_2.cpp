fb262ca2581d90c10bc5fe6ff3a5a9da4f4c15b9
istrokan <65655269+istrokan@users.noreply.github.com>
2020-05-20 13:05:09 +0300

    2 лаба
    заливаю 2 лабу

---
+++ Lab2/SP_2/SP_2.cpp
@@ -0,0 +1,325 @@
+﻿#include "pch.h"
+
+#include <string>
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <string.h>
+#include <limits>
+#include <vector>
+
+#define BUFFER_SIZE 2048
+#define NOTE_TEXT_CHARS_NUMBER 80
+#define NOTE_TEXT_SIZE NOTE_TEXT_CHARS_NUMBER+1
+#define FILE_NAME L"notes.txt"
+
+using namespace std;
+
+struct Note {
+	int number;
+	FILETIME creation_time;
+	WCHAR text[NOTE_TEXT_SIZE];
+	int update_count;
+};
+
+struct NoteHeader {
+	int not_empty_notes_number;
+	int file_size;
+};
+
+FILETIME get_now_file_time();
+
+void primary_init(HANDLE, int);
+
+void print_menu(NoteHeader note_header);
+
+void perform_action(int, NoteHeader*, HANDLE, int);
+
+void show_all_notes(HANDLE file_handle, int notes_number);
+
+void create_note(HANDLE, int, int);
+
+int get_new_note_position(HANDLE, int);
+
+void get_note_text_from_user(const WCHAR* message, WCHAR* text);
+
+void modify_note(HANDLE, int, int);
+
+void delete_note(HANDLE, int, int);
+
+int get_note_number_from_user(const WCHAR* message, int notes_size);
+
+void update_note_header(HANDLE, bool);
+
+void save_note_to_file(HANDLE, Note, int);
+
+void get_note_from_file(HANDLE, Note*, int);
+
+const LPCWSTR file_name = L"notes.txt";
+
+int main(int argc, char * argv[])
+{
+	if (argc != 2) {
+		printf("Incorrect arguements number.\n");
+		return 1;
+	}
+
+	const int notes_number = atoi(argv[1]);
+
+	//Open file
+	HANDLE file_handle = CreateFile(file_name, GENERIC_READ | GENERIC_WRITE, 0, NULL,
+		OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (file_handle == INVALID_HANDLE_VALUE) {
+		printf("ERROR %x \n", GetLastError());
+		CloseHandle(file_handle);
+		exit(2);
+	}
+
+	LARGE_INTEGER file_size = { 0 };
+	GetFileSizeEx(file_handle, &file_size);
+	printf("ERROR %x \n", GetLastError());
+
+	if (file_size.QuadPart == 0) {
+		primary_init(file_handle, notes_number);
+	}
+
+	//Get user input
+	int menu;
+	do {
+		//Get note header from file
+		DWORD readBytesCount;
+		NoteHeader note_header;
+		SetFilePointer(file_handle, 0, NULL, FILE_BEGIN);
+		ReadFile(file_handle, (LPVOID)&note_header, sizeof(NoteHeader), &readBytesCount, NULL);
+
+		print_menu(note_header);
+		wcin >> menu;
+
+		if (menu < 0 && menu > 3) {
+			wcout << "\nIncorrect item\n";
+		}
+		else {
+			perform_action(menu, &note_header, file_handle, notes_number);
+		}
+	} while (menu != 4);
+
+	CloseHandle(file_handle);
+
+	return 0;
+}
+
+FILETIME get_now_file_time() {
+	FILETIME file_time;
+	SYSTEMTIME system_time;
+
+	GetSystemTime(&system_time);
+	SystemTimeToFileTime(&system_time, &file_time);
+
+	return file_time;
+}
+
+void primary_init(HANDLE file_handle, int notes_number) {
+	NoteHeader note_header;
+	note_header.file_size = sizeof NoteHeader;
+	note_header.not_empty_notes_number = 0;
+
+	Note primary_note;
+	primary_note.update_count = -1;
+
+	DWORD writtenBytesCount;
+
+	//SetFilePointer(file_handle, 0, NULL, FILE_BEGIN);
+	WriteFile(file_handle, (LPVOID)&note_header, sizeof(NoteHeader), &writtenBytesCount, NULL);
+
+	for (int i = 0; i < notes_number; i++)
+	{
+		//SetFilePointer(file_handle, sizeof Note, NULL, FILE_CURRENT);
+		WriteFile(file_handle, (LPVOID)&primary_note, sizeof(Note), &writtenBytesCount, NULL);
+	}
+}
+
+void print_menu(NoteHeader note_header) {
+	wcout << "\n\n* NOTES *\n\n"
+		<< "Files: " << note_header.not_empty_notes_number 
+		<< "\nFile size: " <<  note_header.file_size << "\n\n"
+		<< "[0] - show all notes\n"
+		<< "[1] - create note\n"
+		<< "[2] - modify note by number\n"
+		<< "[3] - delete note by number\n"
+		<< "[4] - exit\n\n";
+}
+
+void perform_action(int menu_item, NoteHeader* note_header, HANDLE file_handle, int notes_number) {
+	int note_number;
+	int note_position;
+	switch (menu_item) {
+	case 0: show_all_notes(file_handle, notes_number);
+		break;
+	case 1:
+		note_position = get_new_note_position(file_handle, notes_number);
+		create_note(file_handle, notes_number, note_position);
+		break;
+	case 2:
+		note_number = get_note_number_from_user(L"\nEnter number of note to modify\n>", notes_number);
+		modify_note(file_handle, notes_number, note_number);
+		break;
+	case 3: 
+		note_number = get_note_number_from_user(L"\nEnter number of note to delete\n>", notes_number);
+		delete_note(file_handle, notes_number, note_number);
+		break;
+	case 4: exit(0);
+		break;
+	}
+}
+
+void show_all_notes(HANDLE file_handle, int notes_number) {
+	SetFilePointer(file_handle, sizeof NoteHeader, NULL, FILE_BEGIN);
+	DWORD readenBytesCount;
+	bool is_note_list_empty = true;
+
+	for (int i = 0; i < notes_number; i++) {
+		Note note;
+		ReadFile(file_handle, (LPVOID)&note, sizeof(Note), &readenBytesCount, NULL);
+		//Show only not deleted notes
+		if (note.update_count != -1) {
+			is_note_list_empty = false;
+			SYSTEMTIME st;
+			FileTimeToSystemTime(&note.creation_time, &st);
+
+			wcout << "\n [" << note.number << "] - |" << st.wDay << "." << st.wMonth << "." << st.wYear
+				<< "|updates number - " << note.update_count << "|\n\t";
+			wprintf(note.text);
+		}
+	}
+
+	if (is_note_list_empty) {
+		wcout << "\nThere is no notes in the file.\n";
+	}
+}
+
+int get_new_note_position(HANDLE file_handle, int notes_number) {
+	int note_position = -1;
+
+	SetFilePointer(file_handle, sizeof NoteHeader, NULL, FILE_BEGIN);
+	DWORD readenBytesCount;
+
+	for (int i = 0; i < notes_number; i++) {
+		Note note;
+		ReadFile(file_handle, (LPVOID)&note, sizeof(Note), &readenBytesCount, NULL);
+
+		//if deleted (empty) note
+		if (note.update_count == -1) {
+			note_position = i;
+			break;
+		}
+	}
+
+	return note_position;
+}
+
+void create_note(HANDLE file_handle, int notes_number, int note_position) {
+	if (note_position == -1) {
+		wcout << "\nYou have already" << notes_number << " notes. Please, delete some to create new one.\n";
+		return;
+	}
+
+	WCHAR text[NOTE_TEXT_SIZE];
+	get_note_text_from_user(L"\nEnter the text for a new note\n", text);
+
+	Note note;
+	wcscpy(note.text, text);
+	note.update_count = 0;
+	note.creation_time = get_now_file_time();
+	note.number = note_position;
+
+	update_note_header(file_handle, false);
+	save_note_to_file(file_handle, note, note_position);
+}
+//"\nEnter new text for note <" << note->number << ">\n"
+void get_note_text_from_user(const WCHAR* message, WCHAR* text) {
+	do {
+		wcout << message;
+		wcin.ignore(INT_MAX, '\n');
+		wcin.getline(text, NOTE_TEXT_SIZE);
+	} while (!wcscmp(text, L""));
+}
+
+void modify_note(HANDLE file_handle, int notes_number, int note_position) {
+	Note note;
+	get_note_from_file(file_handle, &note, note_position);
+	//if note deleted (empty)
+	if (note.update_count == -1) {
+		wcout << "\nThere is no existing note with such number\n\n";
+		return;
+	}
+
+	WCHAR new_text[NOTE_TEXT_SIZE];
+	get_note_text_from_user(L"\nEnter new text for the note\n", new_text);
+
+	wcscpy(note.text, new_text);
+	note.update_count++;
+
+	save_note_to_file(file_handle, note, note_position);
+	wcout << "\nNote has been updated!\n\n";
+}
+
+void delete_note(HANDLE file_handle, int notes_number, int note_position) {
+	Note note;
+	get_note_from_file(file_handle, &note, note_position);
+
+	if (note.update_count == -1) {
+		wcout << "\nThere is no existing note with such number\n\n";
+		return;
+	}
+
+	wcscpy(note.text, L"");
+	note.update_count = -1;
+
+	save_note_to_file(file_handle, note, note_position);
+	update_note_header(file_handle, true);
+	wcout << "\nNote has been deleted!\n\n";
+}
+
+void update_note_header(HANDLE file_handle, bool is_note_deleted) {
+	DWORD readenBytesCount, writtenBytesCount;
+	NoteHeader note_header;
+	SetFilePointer(file_handle, 0, NULL, FILE_BEGIN);
+	ReadFile(file_handle, (LPVOID)&note_header, sizeof(NoteHeader), &readenBytesCount, NULL);
+
+	if (is_note_deleted) {
+		note_header.file_size -= sizeof Note;
+		note_header.not_empty_notes_number--;
+	}
+	else {
+		note_header.file_size += sizeof Note;
+		note_header.not_empty_notes_number++;
+	}
+	
+
+	SetFilePointer(file_handle, 0, NULL, FILE_BEGIN);
+	WriteFile(file_handle, (LPVOID)&note_header, sizeof(NoteHeader), &writtenBytesCount, NULL);
+}
+
+void save_note_to_file(HANDLE file_handle, Note note, int note_position) {
+	DWORD writtenBytesCount;
+	SetFilePointer(file_handle, sizeof NoteHeader + (sizeof Note * note_position), NULL, FILE_BEGIN);
+	WriteFile(file_handle, (LPVOID)&note, sizeof(Note), &writtenBytesCount, NULL);
+}
+
+void get_note_from_file(HANDLE file_handle, Note* note, int note_position) {
+	SetFilePointer(file_handle, sizeof NoteHeader + (sizeof Note * note_position), NULL, FILE_BEGIN);
+	DWORD readenBytesCount;
+	ReadFile(file_handle, (LPVOID)note, sizeof(Note), &readenBytesCount, NULL);
+}
+
+int get_note_number_from_user(const WCHAR* message, int notes_size) {
+	int number;
+	do {
+		wcout << message;
+		wcin >> number;
+	} while (number < 0 || number > notes_size);
+
+	return number;
+}
+
---
+++ Lab2/SP_2_2/SP_2_2.cpp
@@ -0,0 +1,376 @@
+﻿#include "pch.h"
+#include <string>
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+#include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sstream>
+#include <locale>
+#include <codecvt>
+
+using namespace std;
+
+void print_dir(const WCHAR *root);
+void perfrom_command(wstring command, vector<wstring> parameters, WCHAR *path);
+void change_directory(vector<wstring> parameters, WCHAR *path);
+void copy_file(vector<wstring> parameters, wstring path);
+void create_directory(vector<wstring> parameters, wstring path);
+void remove_directory(vector<wstring> parameters, wstring path);
+void display_file_info(vector<wstring> parameters, wstring path);
+wstring replace_all(const wstring &message, const wstring &pattern, const wstring &replace);
+int is_directory(wstring path);
+vector<wstring> split(wstring str_to_split, WCHAR separator);
+bool is_file_exists(const wstring name);
+LPWSTR wstring_to_lpwstr(const wstring& s);
+string wstring_to_string(const wstring& s);
+string filetime_to_string(FILETIME ft);
+string bytes_count_to_stirng(DWORD bytes_count);
+
+#define MAX_PATH_SYMBOL_COUNT 260
+#define MAX_COMMANMD_SYMBOL_COUNT 10
+
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
+{
+	WCHAR path[MAX_PATH] = L"D:";
+
+	cout << "\n* File manager *\n\n";
+
+	WCHAR input[MAX_PATH_SYMBOL_COUNT + MAX_COMMANMD_SYMBOL_COUNT + 2];
+	wstring command;
+	vector<wstring> parameters;
+	//WCHAR command[MAX_COMMANMD_SYMBOL_COUNT + 1];
+	//WCHAR parametr[MAX_PATH_SYMBOL_COUNT + 1];
+	do {
+		wcin.getline(input, sizeof input);
+		wstring input_wstr = input;
+
+		int end_command_index = input_wstr.find(L" ");
+		command = input_wstr.substr(0, end_command_index);
+		parameters = split(input_wstr.substr(end_command_index + 1), L' ');
+
+
+
+		perfrom_command(command, parameters, path);
+	} while (command.compare(L"exit") != 0);
+
+}
+
+void perfrom_command(wstring command, vector<wstring> parameters, WCHAR *path) {
+	wprintf(L"\n");
+
+	if (command.compare(L"ls") == 0) {
+		print_dir(path);
+	}
+	else if (command.compare(L"pwd") == 0) {
+		wprintf(L"%s\n", path);
+	}
+	else if (command.compare(L"cd") == 0) {
+		change_directory(parameters, path);
+	}
+	else if (command.compare(L"cp") == 0) {
+		copy_file(parameters, wstring(path));
+	}
+	else if (command.compare(L"mkdir") == 0) {
+		create_directory(parameters, wstring(path));
+	}
+	else if (command.compare(L"rmdir") == 0) {
+		remove_directory(parameters, wstring(path));
+	}
+	else if (command.compare(L"finfo") == 0) {
+		display_file_info(parameters, wstring(path));
+	}
+
+	wprintf(L"\n");
+}
+
+void print_dir(const WCHAR *path) {
+	WCHAR temp[MAX_PATH] = { '\0' };
+	wsprintf(temp, L"%s\\*", path);
+
+	WIN32_FIND_DATA fd;
+	HANDLE handle = FindFirstFile(temp, &fd);
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if (wcscmp(L".", fd.cFileName) != 0 && wcscmp(L"..", fd.cFileName) != 0)
+			{
+				if (fd.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY)
+					wprintf(L"dir \t%s\\%s\n", path, fd.cFileName);
+				else 
+					wprintf(L"file \t%s\\%s\n", path, fd.cFileName);
+			}
+		} while (FindNextFile(handle, &fd));
+	}
+	FindClose(handle);
+}
+
+void change_directory(vector<wstring> parameters, WCHAR *path) {
+	if (parameters.size() != 1) {
+		wprintf(L"Incorrect parameters. Structure: cd <path>");
+		return;
+	}
+
+	wstring wpath = wstring(path);
+	wstring parameter = parameters[0];
+	parameter = replace_all(parameter, L"/", L"\\");
+
+	if (parameter.compare(L"~") == 0) {
+		wcscpy(path, L"");
+		return;
+	} else if (parameter.find(L"..") != -1) {
+		int index_of_parent_dir_end = wpath.find_last_of(L"\\");
+		if (index_of_parent_dir_end != -1) {
+			wpath = wpath.substr(0, index_of_parent_dir_end);
+		}
+		//remove ".." from parameter
+		parameter = parameter.substr(2);
+	}
+
+	if (wpath.empty()) wpath = L"C:";
+	wstring new_path = wpath + parameter;
+
+	switch (is_directory(new_path)) {
+		case 0: wprintf(L"cannot access %s\n", new_path.c_str());
+			break;
+		case 1: 
+			wcscpy(path, new_path.c_str());
+			break;
+		case 2:
+			wprintf(L"%s is not a directory\n", new_path.c_str());
+			break;
+	}		
+}
+
+
+void copy_file(vector<wstring> parameters, wstring path) {
+	if (parameters.size() != 2) {
+		wprintf(L"Incorrect parameters. Structure: cp <file_name> <copied_file_name>\ncp <file_name> <copied_file_full_path>\ncp <file_name> <copied_folder>\n");
+		return;
+	}
+
+	//Process source path
+	wstring source_file_path = path + L"\\" + parameters[0];
+	bool is_source_file_exists;
+	switch (is_directory(source_file_path))
+	{
+		case 0: 
+			wprintf(L"cannot access %s\n", source_file_path.c_str());
+			is_source_file_exists = false;
+			break;
+		case 1:
+			wprintf(L"%s is a directory\n", source_file_path.c_str());
+			is_source_file_exists = false;
+			break;
+		case 2:
+			is_source_file_exists = is_file_exists(source_file_path);
+			break;
+	}
+
+	if (!is_source_file_exists) {
+		wprintf(L"\nIncorrect source path. Try again.\n");
+		return;
+	}
+
+	//Process destination path
+	wstring destination = parameters[1];
+	destination = replace_all(destination, L"/", L"\\");
+
+	wstring destination_folder = L"";
+
+	if (destination.find(L".") == -1) {
+		destination_folder = destination;
+	}
+	else if (destination.find(L"\\") != -1) {
+		destination_folder = destination.substr(0, destination.find_last_of(L"\\"));
+	}
+
+	is_source_file_exists = true;
+	if (!destination_folder.empty()) {
+		wstring destination_folder_path = path + L"\\" + destination_folder;
+		switch (is_directory(destination_folder_path))
+		{
+		case 0:
+			wprintf(L"cannot access %s\n", destination_folder_path.c_str());
+			is_source_file_exists = false;
+			break;
+		case 2:
+			wprintf(L"%s is a directory\n", destination_folder_path.c_str());
+			is_source_file_exists = false;
+			break;
+		}
+	}
+
+	if (!is_source_file_exists) {
+		wprintf(L"\nIncorrect destination path. Try again.\n");
+		return;
+	}
+
+	// if second parameter has new file name
+	if (destination.find(L".") != -1) {
+		wstring destination_path = path + L"\\" + destination;
+		CopyFile(source_file_path.c_str(), destination_path.c_str(), FALSE);
+	}
+	else {
+		wstring destination_path = wstring(path + L"\\" + destination.c_str() + L"\\" + parameters[0]);
+		CopyFile(source_file_path.c_str(), destination_path.c_str(), FALSE);
+	}
+}
+
+void create_directory(vector<wstring> parameters, wstring path) {
+	if (parameters.size() != 1) {
+		wprintf(L"Incorrect parameters. Structure: mkdir <directory_path>\n");
+		return;
+	}
+
+	wstring directory_name = parameters[0];
+	directory_name = replace_all(directory_name, L"/", L"\\");
+	LPWSTR directory_full_name = wstring_to_lpwstr(path + L"\\" + directory_name);
+
+	if (!CreateDirectory(directory_full_name, NULL)) {
+		if (is_directory(directory_full_name) == 1)
+			cout << "Directory is already exists\n";
+		else
+			cout << "Creating directory error\n";
+	}
+}
+
+void remove_directory(vector<wstring> parameters, wstring path) {
+	if (parameters.size() != 1) {
+		wprintf(L"Incorrect parameters. Structure: rmdir <directory_path>\n");
+		return;
+	}
+
+	wstring directory_name = parameters[0];
+	directory_name = replace_all(directory_name, L"/", L"\\");
+	string directory_full_name = wstring_to_string(path + L"\\" + directory_name);//.c_str();
+
+	if (!RemoveDirectoryA(directory_full_name.c_str())) {
+		cout << "Removing directory error\n";
+	}
+}
+
+void display_file_info(vector<wstring> parameters, wstring path) {
+	if (parameters.size() != 1) {
+		wprintf(L"Incorrect parameters. Structure: finfo <file_name>\nfinfo <file_path>\n");
+		return;
+	}
+
+	wstring file_name = parameters[0];
+	file_name = replace_all(file_name, L"/", L"\\");
+	wstring full_path;
+
+	full_path = path + L"\\" + file_name;
+
+	if (is_directory(full_path) == 0) {
+		wprintf(L"Can't access file\n");
+	} else {
+		WIN32_FIND_DATA fd;
+		HANDLE handle = FindFirstFile(full_path.c_str(), &fd);
+		if (handle != INVALID_HANDLE_VALUE)
+		{
+			wprintf(L"full path - %s\n", full_path.c_str());
+			printf("creation time - %s\n", filetime_to_string(fd.ftCreationTime).c_str());
+			printf("last access time - %s\n", filetime_to_string(fd.ftLastAccessTime).c_str());
+			printf("last write time - %s\n", filetime_to_string(fd.ftLastWriteTime).c_str());
+			wprintf(L"full name - %s\n", fd.cFileName);
+			printf("file Size - %s\n", bytes_count_to_stirng((fd.nFileSizeHigh * (MAXDWORD + 1)) + fd.nFileSizeLow).c_str());
+		}
+		else {
+			wprintf(L"Can't access file\n");
+		}
+		FindClose(handle);
+	}
+}
+
+wstring replace_all(const wstring &message, const wstring &pattern, const wstring &replace)
+{
+	wstring result = message;
+	int pos = 0;
+	int offset = 0;
+
+	while ((pos = result.find(pattern, offset)) != wstring::npos)
+	{
+		result.replace(result.begin() + pos, result.begin() + pos + pattern.size(), replace);
+		offset = pos + replace.size();
+	}
+
+	return result;
+}
+
+// return value: 
+// 0 - Can't access
+// 1 - is a directory
+// 2 - is not a directory
+int is_directory(wstring path) {
+	struct _stat64i32 info;
+	if (_wstat(path.c_str(), &info) != 0)
+		return 0;
+	else if (info.st_mode & S_IFDIR)
+		return 1;
+	else
+		return 2;
+}
+
+vector<wstring> split(wstring str_to_split, WCHAR separator) {
+	std::wstring temp;
+	std::vector<wstring> parts;
+	std::wstringstream wss(str_to_split);
+	while (getline(wss, temp, separator))
+		parts.push_back(temp);
+
+	return parts;
+}
+
+bool is_file_exists(const wstring name) {
+	struct _stat64i32 buffer;
+	return (_wstat(name.c_str(), &buffer) == 0);
+}
+
+LPWSTR wstring_to_lpwstr(const wstring& s)
+{
+	LPWSTR ws = new wchar_t[s.size() + 1];
+	copy(s.begin(), s.end(), ws);
+	ws[s.size()] = 0;
+	return ws;
+}
+
+string wstring_to_string(const wstring& s)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(s).c_str();
+}
+
+string filetime_to_string(FILETIME ft) {
+	SYSTEMTIME st, st_local;
+	DWORD dwRet;
+
+	// Convert the last-write time to local time.
+	FileTimeToSystemTime(&ft, &st);
+	SystemTimeToTzSpecificLocalTime(NULL, &st, &st_local);
+
+	char buf[256];
+	snprintf(buf, 256, "%lu.%lu.%lu %lu:%lu", st_local.wDay, st_local.wMonth, st_local.wYear, st_local.wHour, st_local.wMinute);
+	return string(buf);
+}
+
+string bytes_count_to_stirng(DWORD bytes_count) {
+	char buf[32];
+	if (bytes_count < 1024) {
+		snprintf(buf, 32, "%lu B", bytes_count);
+	}
+	else if (bytes_count < 1024 * 1024) {
+		snprintf(buf, 32, "%lu KB",bytes_count/1024);
+	}
+	else if (bytes_count < 1024 * 1024 * 1024) {
+		snprintf(buf, 32, "%lu MB", bytes_count / 1024 / 1024);
+	}
+	else if (bytes_count < 1024 * 1024 * 1024 * 1024) {
+		snprintf(buf, 32, "%lu GB", bytes_count / 1024 / 1024 / 1024);
+	}
+	return string(buf);
+}
